8942c572c6f672a2841a2cce1201c4914b6826f5
Vincent Yu <vincenty.chn@gmail.com>
2019-02-24 23:50:58 -0600

    v1.0
    Finish  basic layout

---
+++ AlbumArt/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  AlbumArt
+//
+//  Created by Vincent Yu on 2/24/19.
+//  Copyright © 2019 Vincent Yu. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ AlbumArt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlbumArt
+//
+//  Created by Vincent Yu on 2/24/19.
+//  Copyright © 2019 Vincent Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
